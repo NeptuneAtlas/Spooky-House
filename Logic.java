3fe8d26c49dcc738c2963869a0ba712bae73b58e
Gerald Atilus <49931560+NeptuneAtlas@users.noreply.github.com>
2021-05-21 07:40:34 -0700

    Create Logic.java

---
+++ Logic.java
@@ -0,0 +1,288 @@
+import javax.swing.JOptionPane;
+public class Logic {// this class is just to handle any underlying logic and to just make code more "clean"
+public void greeting0() {
+    System.out.println("Hello, what is your name?");
+
+}
+
+public void greeting() {
+JOptionPane.showMessageDialog(null, "In front of you is a door that is broken and full of mold \n*Enters the house that is filled with cobwebs*");
+//JOptionPane.showMessageDialog(null,"*Enters the house that is filled with cobwebs*");
+}
+public void choice() {
+    JOptionPane.showMessageDialog(null,"*Enters living room");
+    //System.out.println("Would you like to search room or go to bathroom? ");
+}
+public void choice1() {
+    JOptionPane.showMessageDialog(null,"*Enters dinning room*\nWould you like to search room or enter the kitchen?");
+//System.out.println("Would you like to search room or enter the kitchen? ");
+}
+public void choice2() {
+    JOptionPane.showMessageDialog(null,"*Enters kitchen*\nWould you like to search room or enter the pantry");
+//System.out.println("Would you like to search room or enter the pantry");
+}
+public void choice3() {
+    JOptionPane.showMessageDialog(null,"You look into this moist abyss to open it and find some delicious soul food\nThe room grows darker by every second!!!");
+//System.out.println("The room grows darker by every second!!!");
+    JOptionPane.showMessageDialog(null,"You have reached a dead end.\nThe dishes and glasses start flying at you as soon as you open the door. You get hit in the head and feel yourself start moving towards a light*");
+//System.out.println("*The dishes and glasses start flying at you as soon as you open the door. You get hit in the head and feel yourself start moving towards a light*");
+}
+public void choice4() {
+    JOptionPane.showMessageDialog(null,"A gust a wind blows you into the pantry.\n*You open it up and a recipe for chocolate devils food cake appears out of no where*");
+//System.out.println("*You open it up and a recipe for chocolate devils food cake appears out of no where*");
+    JOptionPane.showMessageDialog(null,"You continue to search the room\nFlies up in the air as soon as you touch it");
+//System.out.println("Flies up in the air as soon as you touch it");
+}
+
+public void choice5() {
+    JOptionPane.showMessageDialog(null,"You head towards the master bedroom");
+
+}
+
+public void choice6() {
+    JOptionPane.showMessageDialog(null,"You are inside the master bedroom\nWould you like to search room, enter the Master Bathroom, bedroom 1, or bedroom 2?");
+    //System.out.println("Would you like to search room, enter the Master Bathroom, bedroom 1, or bedroom 2? ");
+
+}
+
+public void outcome7() {
+    JOptionPane.showMessageDialog(null,"Rub the lamp and a genie pops out who says he’ll grant you 3 wishes\nSuddenly hear singing in the shower, but no one is there");
+    //System.out.println("Suddenly hear singing in the shower, but no one is there");
+}
+public void outcome() {
+    JOptionPane.showMessageDialog(null,"You open up this brown thing that puts up quite a struggle\nGhost escapes and scares you to death and you fall down to the ground and die");
+    //System.out.println("Ghost escapes and scares you to death and you fall down to the ground and die");
+}
+public void outcome1() {
+    JOptionPane.showMessageDialog(null,"You look inward towards this relective object\nYou see a bloody face looking back at you\nRoom suddenly steams up and you feel fingers touching the back of your neck");
+    //System.out.println("You see a bloody face looking back at you");
+    //System.out.println("Room suddenly steams up and you feel fingers touching the back of your neck");
+}
+
+public void outcome2() {
+    JOptionPane.showMessageDialog(null,"Light up by themselves and see a death shadow");
+    
+}
+public void outcome3() {
+    JOptionPane.showMessageDialog(null,"You find the cursed Hope Diamond and feel your doom");
+}
+
+public void outcome4() {
+    JOptionPane.showMessageDialog(null,"Rub the lamp and a genie pops out who says he’ll grant you 3 wishes\nSuddenly hear singing in the shower, but no one is there");
+    //System.out.println("Suddenly hear singing in the shower, but no one is there");
+}
+public void mapHouse() {
+    System.out.println("                    	.-----.\n" +
+   		 "                     	.'   	`.\n" +
+   		 "                    	:  	^v^  :\n" +
+   		 "                    	:       	:\n" +
+   		 "                    	'       	'\n" +
+   		 "     	|~    	www   `.   	.'\n" +
+   		 "    	/.\\   	/#^^\\_   `-/\\--'\n" +
+   		 "   	/#  \\ 	/#%	\\   /# \\\n" +
+   		 "  	/#%   \\   /#%______\\ /#%__\\\n" +
+   		 " 	/#% 	\\   |= I I ||  |- |\n" +
+   		 " 	~~|~~~|~~   |_=_-__|'  |[]|\n" +
+   		 "   	|[] |_______\\__|/_ _ |= |`.\n" +
+   		 "^V^	|-  /= __   __	/-\\|= | :;\n" +
+   		 "   	|= /- /\\/  /\\/   /=- \\.-' :;\n" +
+   		 "   	| /_.=========._/_.-._\\  .:'\n" +
+   		 "   	|= |-.'.- .'.- |  /|\\ |.:'\n" +
+   		 "   	\\  |=|:|= |:| =| |~|~||'|\n" +
+   		 "    	|~|-|:| -|:|  |-|~|~||=|  	^V^\n" +
+   		 "    	|=|=|:|- |:|- | |~|~|| |\n" +
+   		 "    	| |-_~__=_~__=|_^^^^^|/___\n" +
+   		 "    	|-(=-=-=-=-=-(|=====/=_-=/\\\n" +
+   		 "    	| |=_-= _=- _=| -_=/=_-_/__\\ \n" +
+   		 "    	| |- _ =_-  _-|=_- |]#| I II\n" +
+   		 "    	|=|_/ \\_-_= - |- = |]#| I II\n" +
+   		 "    	| /  _/ \\. -_=| =__|]!!!I_II!!\n" +
+   		 "   	_|/-'/  ` \\_/ \\|/' _ ^^^^`.==_^.\n" +
+   		 " 	_/  _/`-./`-; `-.\\_ / \\_'\\`. `. ===`.\n" +
+   		 "	/ .-'  __/_   `.   _/.' .-' `-. ; ====;\\\n" +
+   		 "   /.   `./	\\ `. \\ / -  /  .-'.' ====='  >\n" +
+   		 "  /  \\  /  .-' `--.  / .' /  `-.' ======.' /\n" +
+   		 "");
+    //System.out.println("Suddenly hear singing in the shower, but no one is there");
+}
+public void mapLiving() {
+    System.out.println("::::==========:::::::::::::::::::::::::::::::::::::::::::::::::::::::\n" +
+   		 ":::::::=========::::::.---------------.:::::::::::::::::::::::::::::::\n" +
+   		 ":::=============::::::| .-----------. |:::::::::::::::::::::::::::::\n" +
+   		 "::::==========::::::::| | === == == | |:::::::::::::::::::::::::::::::::\n" +
+   		 "::::==========::::::::| | 260 ED OO | |:::::::::::::::::::::::::::::::\n" +
+   		 ":::::::=========='::::| |  urgent!  | |:::::::::::::::::::::::::::::\n" +
+   		 ":::===========::::::::| |___________| |::::::((;):::::::::::::::::::::\n" +
+   		 "\"\"\"\"============\"\"\"\"\"\"|___________oo__|\"\")\"\"\"\";(\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\n" +
+   		 "  ==========='       	___)___(___,o  (   .---._\n" +
+   		 " 	===========    	|___________| 8  \\  |TEA|_)	.+-------+.\n" +
+   		 "  ===========                 	o8o8	) |___|	.' |_______| `.\n" +
+   		 "	=============  	__________8___	(      	/  /     	\\  \\\n" +
+   		 " |\\`==========='/|   .'= --------- --`.   `.   	|\\ /       	\\ /|\n" +
+   		 " | \"-----------\" |  / ooooooooooooo  oo\\   _\\_ 	| \"-------------\" |\n" +
+   		 " |______I_N______| /  oooooooooooo[] ooo\\  |=| 	|_______OUT_______|\n" +
+   		 "              	/ O O =========  O OO  \\ \"-\"   .-------,\n" +
+   		 "              	`\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"'  	/~~~~~~~/\n" +
+   		 "_______________________________________________/_   ~~~/_______________\n" +
+   		 "hjw.......................................... \\/_____/..desk at 17:30..");    
+}
+
+public void mapBath() {
+    System.out.println("    	o	.   _ 	.\n" +
+   		 "      	. 	(_)     	o\n" +
+   		 "   o  	____        	_   	o\n" +
+   		 "  _   ,-/   /)))  .   o  (_)   .\n" +
+   		 " (_)  \\_\\  ( e( 	O         	_\n" +
+   		 " o   	\\/' _/   ,_ ,  o   o	(_)\n" +
+   		 "  . O	_/ (_   / _/  	.  ,    	o\n" +
+   		 " 	o8o/	\\\\_/ / ,-.  ,oO8/( -TT\n" +
+   		 "	o8o8O | } }  / /   \\Oo8OOo8Oo|| 	O\n" +
+   		 "   Oo(\"\"o8\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"8oo\"\"\"\"\"\"\")\n" +
+   		 "  _   `\\`'              	`'   /'   o\n" +
+   		 " (_)	\\                   	/	_   .\n" +
+   		 "  	O  \\       	_     	/	(_)\n" +
+   		 "o   . 	`-. .----<(o)_--. .-'\n" +
+   		 "   --------(_/------(_<_/--\\_)--------");    
+}
+
+public void mapKitchen() {
+    System.out.println("_____________________________________________________\n" +
+   		 "____________________________________________________\\\\\n" +
+   		 "|.-------.-------.|_.----._.----._|.-------.-------.\\\\\\\n" +
+   		 "|]   	|   	[|   	.   	|]   	|   	[ \\\\\\\n" +
+   		 "||   	|   	|| 	.':'. 	||   	|   	|  |\\\\\n" +
+   		 "||   	|   	||	.' : '.	||   	|   	|  |\\\\\\\n" +
+   		 "|| 	(O|O) 	||   .'  :  '.   || 	(O|O) 	|  | \\\\\\\n" +
+   		 "||   	|   	||  .'===:==='.  ||   	|   	| O|  |\\\\\n" +
+   		 "||   	|   	||=='	:	'==||   	|   	|  |  |\\\\\\\n" +
+   		 "|]   	|   	[|  )	:	(  |]   	|   	[  |O | \\\\\\\n" +
+   		 "||_______|_______||\"\" ____:_____\"\"||_______|_______|  |  |  |\\\\\n" +
+   		 "'-----------------'_______________'----------------'  |  |  |\\\\\\\n" +
+   		 "|.--------.  |	'---------------'  (o)______)(0)  \\ |  |  | \\\\\\\n" +
+   		 "||    	|::|_________________________________())___\\|  | O|  \\\\\\______\n" +
+   		 "||    	|::|-----______*!*______-------------))( .'.\\  |  |   | _____ |\n" +
+   		 "||________|::|  _ /   	'   	\\  _    	_   (__.'\\ |  |O  || 	||\n" +
+   		 "|____________| _  \\_______________/ 	_       	(_.'\\|  |   ||  _  ||\n" +
+   		 " ___________________________________________  	_  (___\\  |   || 	||\n" +
+   		 "||.-----.|.------.|.-.-.--.--.-.-.||.-----.||\\   _    	\\ |   || 	||\n" +
+   		 "||| === ||| ==== ||| | |  |  | | |||| === ||| \\ 	_  	\\|   ||	_||\n" +
+   		 "||'-----'|'------'|'-'-'--'--'-'-'||'-----'||. \\      	_ \\   || 	||\n" +
+   		 "||.-----.|.------.|.------.------.||.-----.|| `|\\   	_	\\  || _   ||\n" +
+   		 "||| === |||  	|||  	|  	|||| === |||\\ | \\  _     	\\ ||_____||\n" +
+   		 "||'-----'|]  	||]  	|  	[||'-----'|| \\|. \\    	_   \\|_______|\n" +
+   		 "||.-----.||	(O|||	(O|O)	|||.-----.||  | `|\\               	||\n" +
+   		 "||| === |||  	|||  	|  	|||| === |||  |\\ | \\__________________||\n" +
+   		 "||| 	||]  	||]  	|  	[||| 	||| O| \\|. |  _____________  ||\n" +
+   		 "||'-----'||______|||______|______|||'-----'||  |  | `| |         	| ||\n" +
+   		 "||LGB____|________|_______________||_______||  |O |\\ | |   _     	| ||\n" +
+   		 "''-----------------------------------------' \\ |  | \\| |      	_  | ||\n" +
+   		 "   ____             	_______           	\\|  |  | |   	_ 	| ||\n" +
+   		 "       	_________              	______   \\  |O | |         	| ||\n" +
+   		 "                                            	\\ |  | |   _  	_  | ||\n" +
+   		 "                            	_________    	\\|  | |         	| ||\n" +
+   		 "  	___________    	__                   	\\  | | _    	_  | ||\n" +
+   		 "	__                          	_________  	\\ | |_____________| ||\n" +
+   		 "           	___________                      	\\|_________________||\n" +
+   		 "");    
+}
+public void mapPantry() {
+    System.out.println(" #\n" +
+   		 "  :#:\n" +
+   		 "  : :\n" +
+   		 "  : :\n" +
+   		 ".'   '.\n" +
+   		 ":_____:  .___. .___.\n" +
+   		 "| 	|  |   | |   |    	" +
+   		 "| 	|  '. .' '. .'             	/\n" +
+   		 "| 	|	| 	|  	.--''''''--.\n" +
+   		 "|_____|	| 	| 	|'''''/  	|\n" +
+   		 ":_____:   -'-   -'-	'''''/_...--'|\n" +
+   		 "                       	|__...--'  ");    
+}
+public void mapMasterBedroom() {
+System.out.println(" _  	()          	()  	_\n" +
+   	 "       	/ \\ 	||______________|| 	/ \\\n" +
+   	 "      	/___\\	|            	|	/___\\\n" +
+   	 "        	|  	|  	~@@~  	|  	|\n" +
+   	 "       	(_) 	|_______  _______| 	(_)\n" +
+   	 "    	___/_\\___  {_______}{_______}  ___/_\\___\n" +
+   	 "     	|__~__|   %%%%%%%%%%%%%%%%%%   |__~__|\n" +
+   	 "  	___|_____|__%%%%%%%%%%%%%%%%%%%%__|_____|___\n" +
+   	 "     	| 	| %%%%%%%%%%%%%%%%%%%%%% | 	|\n" +
+   	 "      	`=====%%%%%%%%%%%%%%%%%%%%%%%%=====`\n" +
+   	 "     	`=====%%%%%%%%%%%%%%%%%%%%%%%%%%=====`\n" +
+   	 "    	`=====%%%%%%%%%%%%%%%%%%%%%%%%%%%%=====`\n" +
+   	 "   	`=====/||||||||||||||||||||||||||||\\=====`\n" +
+   	 "  	`======||||||||||||||||||||||||||||||======`\n" +
+   	 " 	`=======|||||||||||||||||||||||||||lc|=======`\n" +
+   	 "	`==============================================`\n" +
+   	 "   `================================================`\n" +
+   	 "  `==================================================`\n" +
+   	 " `====================================================`");    
+}
+public void mapMasterBathroom() {
+System.out.println("            	___\n" +
+   	 "                  	.' _ '.\n" +
+   	 "                 	/ /` `\\ \\\n" +
+   	 "                 	| |   [__]\n" +
+   	 "                 	| |	{{\n" +
+   	 "                 	| |	}}\n" +
+   	 "              	_  | |  _ {{\n" +
+   	 "  	___________<_>_| |_<_>}}________\n" +
+   	 "      	.=======^=(___)=^={{====.\n" +
+   	 "     	/ .----------------}}---. \\\n" +
+   	 "    	/ /             	{{	\\ \\\n" +
+   	 "   	/ /              	}} 	\\ \\\n" +
+   	 "  	(  '========================='  )\n" +
+   	 "   	'-----------------------------'");    
+}
+public void mapBedroom1() {
+System.out.println(" 	_   ()\n" +
+   	 "() .'_`'.||\n" +
+   	 "||/.| ||\\ |\n" +
+   	 "| /|| || ||     	_   ()\n" +
+   	 "|| || || ||	() .'_`'.||\n" +
+   	 "|| || || ||	||/.| ||\\ |\n" +
+   	 "|| || |I-'`-._ | /|| || ||\n" +
+   	 "||_I'`_.-||-._`'| || || ||\n" +
+   	 "m._ <'_  ||   `|| || || ||\n" +
+   	 "i| `-._`-'|	|| || |I-'|\n" +
+   	 "c) 	`-._`-._||_I'`_.-||\n" +
+   	 "       	`-._` _.-'   ()\n" +
+   	 "           	||   	`\n" +
+   	 "           	()");    
+}
+public void mapBedroom2() {
+System.out.println("__________________________________________________________________________________\n" +
+   	 "_______O____________________________________________O____________________________/\n" +
+   	 "\\______%____________________________________________%___________________________/\n" +
+   	 " \\_____%__o______________________________________o__%__________________________/\n" +
+   	 "  \\____%__%______________________________________%__%_________________________/\n" +
+   	 "  |	%  %                                  	%  %   	[|_|_|_|_|_|] 	|\n" +
+   	 "  |	%  %                                  	%  %   	[_|_|_|_|_|_] 	|\n" +
+   	 "  |	%  %        	~@~@~@~@~@~@~@~       	%  %   	[|_|_|_|_|_|] 	|\n" +
+   	 "  |	%  %       	*(=(=(=(=)=)=)=)*      	%  %   	[_|_|_|_|_|_] 	|\n" +
+   	 "  |	%  %    	_   :|!|:!|!|!:|!|:   _   	%  %	_  [|_|~@@@~|_|]  _  |\n" +
+   	 "  |	%  %   	/_\\  !:|:!:}|{:!:|:!  /_\\  	%  %   /_\\ [_|_|_|_|_|_] /_\\ |\n" +
+   	 "  |	%  %   	=|=  |!:|!} | {!|:!|  =|=  	%  %   =|= [|_|_|_|_|_|] =|= |\n" +
+   	 "  |	%  %        	!:!:}  |  {:!:!       	%  %   	[_|_|_|_|_|_] 	|\n" +
+   	 "  |	%  %        	~@~'---+---`~@~       	%  %   ___/_|_|_|_|_|_|_\\___ |\n" +
+   	 "  |	%  %        	|:|	|	|:|       	%  %  '====================='|\n" +
+   	 "  |	%  o____________:!:____|____:!:_____.-=---o  %   [_|_]===========[_|_] |\n" +
+   	 "  |	% /                             	\\  	\\ %   [___]       	[___] |\n" +
+   	 "  |	O/___________________________________\\.-=.--\\O   [_|_] o  `(,  o [_|_] |\n" +
+   	 "  |	|_____________________________________~~~~~~~|   [___] |  ( )  | [___] |\n" +
+   	 "  |____!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!___[_|_]/!\\_@@@_/!\\[_lc]_|\n" +
+   	 "  /====!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!=========================\\\n" +
+   	 " /==========_==================================================================\\\n" +
+   	 "/=========/ /\\==================================================================\\\n" +
+   	 "=========/_/ /\\==================================================================\\\n" +
+   	 "=========| \\/ /\\==================================================================\\\n" +
+   	 "=========|\\ \\/ /\\==================================================================\\\n" +
+   	 "=========||\\ \\/ /\\==================================================================\\\n" +
+   	 "=========||=\\ \\/=====================================================================\\\n" +
+   	 "=========||==\\_\\======================================================================\\\n" +
+   	 "=======================================================================================\\\n" +
+   	 "\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"");    
+}
+}
